07e7ae300f1d5d65cf100ca2cc85ad619153db58
Bob Morganstern <bmorganstern@paloaltonetworks.com>
2018-04-03 17:18:40 -0700

    Added updated code to work with ui-core changes - added ButtonSVG, Button2 be rendered in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'ui-core';
-import '../assets/stylesheets/base.scss';
-
+import { Button, Button2, ButtonSVG } from 'ui-core';
 
 const App = ({ name }) => {
-    return (
-        <div>
-            <h1>Hdfasfello, {name}!</h1>
-            <Button>Click Me!</Button>
-        </div>
-    );
+  return (
+    <div>
+      <Button>Click Me!</Button>
+      <h1>Hdfasfello, {name}!</h1>
+      <Button2>Click 2</Button2>
+      <ButtonSVG>SVG Button</ButtonSVG>
+    </div>
+  );
 };
 
 App.propTypes = {
-    name: PropTypes.string,
+  name: PropTypes.string,
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,39 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: [
-    './src/index'
-  ],
-  module: {
-    loaders: [
-      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
+    entry: [
+        './src/index'
+    ],
+    module: {
+        loaders: [
+            { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.s?css$/, loader: 'style-loader!css-loader!sass-loader' },
+        ]
+    },
+    resolve: {
+        extensions: ['.js','.scss']
+    },
+    output: {
+        path: path.join(__dirname, '/dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devtool: 'cheap-eval-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: './src/index.html',
+            filename: './index.html',
+        })
     ]
-  },
-  resolve: {
-    extensions: ['.js','.scss']
-  },
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devtool: 'cheap-eval-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  ]
 };
